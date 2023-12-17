2619cf4c175e05fb4b7a119bf44bff1858055a26
Oleg Shelest <lateexairu@gmail.com>
2023-12-17 17:19:56 +0200

    Update ToTopButton styling

--- src/components/ToTopButton.tsx
+++ src/components/ToTopButton.tsx
@@ -25,7 +25,7 @@ export default function ScrollToTop() {
       onClick={goToTop}
       className={`${
         showTopBtn ? "visible opacity-100" : "invisible opacity-0"
-      } fixed bottom-4 right-4 z-10 rounded-md bg-white transition-opacity duration-300 md:bottom-10 md:right-10`}
+      } fixed bottom-4 right-4 z-10 rounded-md bg-surface-light transition-opacity duration-300 md:bottom-10 md:right-10`}
     >
       <Icons className="h-8 w-8 fill-accent md:h-10 md:w-10" icon="arrowUp" />
     </button>
